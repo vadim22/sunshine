58ba7a6f107f6b835d5ecdf38688296c9c3b69fa
vadim22 <vadim.albul@webquli.com>
2015-02-20 16:03:57 +0200

    the result app 2 Lessens

--- app/src/main/java/com/example/avs/sunshine/MainActivity.java
+++ app/src/main/java/com/example/avs/sunshine/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.avs.sunshine;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -26,7 +33,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -52,48 +59,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        private ArrayAdapter<String> mForecastAdapted;
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            String[] forecastArray = {
-                    "Mon : Sunny : -12°/-3°",
-                    "Tue : Sunny : -11°/-1°",
-                    "Weds : Sunny : -9°/0°",
-                    "Thurs : Partly cloudy : -8°/+2°",
-                    "Fri : Cloudy : -6°/+2°",
-                    "Sat : Cloudy : -3°/+1°",
-                    "Sun : Sunny : -6°/+1°"
-            };
-
-            List<String> weekForecast = new ArrayList<String>(
-                    Arrays.asList(forecastArray));
-       
-            mForecastAdapted = new ArrayAdapter<String>(
-                    getActivity(),
-                    R.layout.list_item_forecast,
-                    R.id.list_item_forecast_textview,
-                    weekForecast
-            );
-
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(mForecastAdapted);
-            return rootView;
-        }
-
-
     }
 }
