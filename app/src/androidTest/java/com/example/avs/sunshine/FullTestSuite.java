747fb916f2427a88b5c27d4dcde83cb72112d542
vadim22 <vadim.albul@webquli.com>
2015-03-22 11:53:41 +0200

    Lesson $B: Content Providers

--- app/src/androidTest/java/com/example/avs/sunshine/FullTestSuite.java
+++ app/src/androidTest/java/com/example/avs/sunshine/FullTestSuite.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.avs.sunshine.data;
+package com.example.avs.sunshine;
 
 import android.test.suitebuilder.TestSuiteBuilder;
 
